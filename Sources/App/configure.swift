8d841ee474363ae9ff859d12b26a382b1ce80911
George <z51683567@livemail.tw>
2023-03-03 01:42:09 +0800

    Checkpoint

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,12 +1,23 @@
 import Vapor
 import Fluent
 import FluentMySQLDriver
+import NIOSSL
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    // basic configurations
+    app.http.server.configuration.hostname = "10.0.43.2"
+    app.http.server.configuration.port = 8080
+    app.http.server.configuration.supportVersions = [.two]
+    
+    // tls configuration
+    let certificate = try NIOSSLCertificate.fromPEMFile("/etc/letsencrypt/live/zyozi.jp/cert.pem").first!
+    let privateKey = try NIOSSLPrivateKey(file: "/etc/letsencrypt/live/zyozi.jp/privkey.pem", format: .pem)
+    app.http.server.configuration.tlsConfiguration = TLSConfiguration.makeServerConfiguration(certificateChain: [.certificate(certificate)], privateKey: .privateKey(privateKey))
+    
     // register routes
     try routes(app)
     
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,11 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    // TLS Certificate well-known
+    app.get(".well-known", "acme-challenge", "Z930vb-dGDYMaSI_rhovvtU9e0CLQzJRZFnEQNCD1yU") { req async -> String in
+        "Z930vb-dGDYMaSI_rhovvtU9e0CLQzJRZFnEQNCD1yU.zZfnBkxMezT5cLIDw2rZXTBKLHac7P3YBmqqYc5-SYk"
+    }
+    
     app.get { req async in
         "It works!"
     }
